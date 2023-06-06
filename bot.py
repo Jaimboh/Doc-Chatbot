66cc42a88500b6a843620064a157b7f2acc1cbe5
Jaimboh <110313204+Jaimboh@users.noreply.github.com>
2023-06-06 17:41:40 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,21 +5,21 @@ api_key = st.secrets["openai"]["api_key"]
 # Set the OpenAI API key as an environment variable
 os.environ["OPENAI_API_KEY"] = api_key
 
-
+import pickle
 from llama_index import download_loader
 from llama_index.node_parser import SimpleNodeParser
 from llama_index import GPTVectorStoreIndex
-from llama_index import LLMPredictor,GPTVectorStoreIndex, PromptHelper, ServiceContext
+from llama_index import LLMPredictor, PromptHelper, ServiceContext
 from langchain import OpenAI
 
 doc_path = './data/'
-index_file = 'index.json'
+index_file = 'index.pkl'
 
 if 'response' not in st.session_state:
     st.session_state.response = ''
 
 def send_click():
-    st.session_state.response  = index.query(st.session_state.prompt)
+    st.session_state.response = index.query(st.session_state.prompt)
 
 index = None
 st.title("Tom's Document Assistant")
@@ -32,13 +32,12 @@ sidebar_placeholder = st.sidebar.container()
 uploaded_file = st.file_uploader("Choose a file")
 
 if uploaded_file is not None:
-
     doc_files = os.listdir(doc_path)
     for doc_file in doc_files:
         os.remove(doc_path + doc_file)
 
     bytes_data = uploaded_file.read()
-    with open(f"{doc_path}{uploaded_file.name}", 'wb') as f: 
+    with open(f"{doc_path}{uploaded_file.name}", 'wb') as f:
         f.write(bytes_data)
 
     SimpleDirectoryReader = download_loader("SimpleDirectoryReader")
@@ -47,7 +46,7 @@ if uploaded_file is not None:
     documents = loader.load_data()
     sidebar_placeholder.header('Current Processing Document:')
     sidebar_placeholder.subheader(uploaded_file.name)
-    sidebar_placeholder.write(documents[0].get_text()[:10000]+'...')
+    sidebar_placeholder.write(documents[0].get_text()[:10000] + '...')
 
     llm_predictor = LLMPredictor(llm=OpenAI(temperature=0, model_name="text-davinci-003"))
 
@@ -62,10 +61,12 @@ if uploaded_file is not None:
         documents, service_context=service_context
     )
 
-    index.save(index_file)
+    with open(index_file, 'wb') as file:
+        pickle.dump(index, file)
 
 elif os.path.exists(index_file):
-    index = GPTVectorStoreIndex.load_from_disk(index_file)
+    with open(index_file, 'rb') as file:
+        index = pickle.load(file)
 
     SimpleDirectoryReader = download_loader("SimpleDirectoryReader")
     loader = SimpleDirectoryReader(doc_path, recursive=True, exclude_hidden=True)
@@ -73,11 +74,11 @@ elif os.path.exists(index_file):
     doc_filename = os.listdir(doc_path)[0]
     sidebar_placeholder.header('Current Processing Document:')
     sidebar_placeholder.subheader(doc_filename)
-    sidebar_placeholder.write(documents[0].get_text()[:10000]+'...')
+    sidebar_placeholder.write(documents[0].get_text()[:10000] + '...')
 
-if index != None:
+if index is not None:
     st.text_input("Ask something: ", key='prompt')
     st.button("Send", on_click=send_click)
     if st.session_state.response:
-        st.subheader("Response: ")
-        st.success(st.session_state.response, icon= "🤖")
+        st.subheader("Response:")
+        st.success(st.session_state.response, icon="🤖")
